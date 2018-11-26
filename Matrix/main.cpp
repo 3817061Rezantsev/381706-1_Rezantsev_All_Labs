0487386c20e558668b96d8f20fe42defe8bf08a3
3817061Rezantsev <rezantsev.sergey@gmail.com>
2018-11-26 18:54:37 +0300

    matrix

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include "../MatrixLib/TMatrix.h"
+using namespace std;
 void main()
 {
-
+	TMatrix<int> a(5), b(5);
+	int i, j;
+	
+	setlocale(LC_ALL, "Russian");
+	cout << "Тестирование программ поддержки представления треугольных матриц"
+		<< endl;
+	for (int k = 0;k < 15;k++) 
+	{
+		a.SetValue(k, k);
+		b.SetValue(k, 2 * k);
+	}
+	TMatrix<int> c = a + b;
+	cout << "Matrix a = " << endl << a << endl;
+	cout << "Matrix b = " << endl << b << endl;
+	cout << "Matrix c = a + b" << endl << c << endl;
+	
 }
--- MatrixLib/TMatrix.h
+++ MatrixLib/TMatrix.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <stdio.h>
 #include <math.h>
 #include "../VectorLib/TVector.h"
 
@@ -8,26 +9,42 @@ template <class T>
 class TMatrix: public TVector<T>
 {
 public:
-	TMatrix(int = 0);
+	TMatrix();
+	TMatrix(int a);
 	TMatrix(TMatrix &A);
-	TMatrix(TVector<TVector<T>> &A);
+	T& element(int i, int j);
+	TMatrix(const TVector<TVector<T>> &A);
 	TMatrix<T> operator+(TMatrix<T> &A);
 	TMatrix<T> operator-(TMatrix<T> &A);
 	TMatrix<T> operator*(TMatrix<T> &A);
 	TMatrix<T>& operator=(TMatrix<T> &A);
-	TMatrix<T>& operator==(TMatrix<T> &A);
+	bool operator==(const TMatrix &A);
 	template <class T>
-	friend std::istream& operator>>(std::istream& A, TMatrix<T>& B);
+	friend std::istream& operator>>(std::istream& A, TVector<T>& B);
 	template <class T>
-	friend std::ostream& operator<<(std::ostream& A, TMatrix<T>& B);
-};
-// ---------------------------------------------------------------------------
+	friend std::ostream& operator<<(std::ostream& A, TVector<T>& B);
+	};
 template <class T>
-TMatrix<T>::TMatrix(int = 0)
+TMatrix<T>::TMatrix()
 {
 	vector = 0;
 	length = 0;
 }
+// ---------------------------------------------------------------------------
+template <class T>
+TMatrix<T>::TMatrix(int a)
+{
+	length = a;
+	if (length != 0)
+	{
+		int n = length * (length + 1) / 2;
+		vector = new T[n];
+		for (int i = 0; i < n; i++)
+			vector[i] = 0;
+	}
+}
+// ---------------------------------------------------------------------------
+
 // ---------------------------------------------------------------------------
 template <class T>
 TMatrix<T>::TMatrix(TMatrix &A)
@@ -45,33 +62,45 @@ TMatrix<T>::TMatrix(TMatrix &A)
 }
 // ---------------------------------------------------------------------------
 template <class T>
-TMatrix<T>::TMatrix(TVector<TVector<T>> &A)
+T& TMatrix<T>::element(int i, int j) 
 {
-	
+	if ((i > j) || (i >= length) || (j >= length) || (i < 0) || (j < 0))	
+	return vector[i*length - i * (i - 1) / 2 + (j - i)];
 }
+// ---------------------------------------------------------------------------
+template <class T>
+TMatrix<T>::TMatrix(const TVector<TVector<T>> &A) :
+	TVector<TVector<T>>(mt) {}
 // ---------------------------------------------------------------------------
 template <class T>
 TMatrix<T> TMatrix<T>::operator+(TMatrix<T> &A)
 {
-	TMatrix<T> S;
-	if (length == A.length)
+	TMatrix<T>* S = new TMatrix<T>(A.GetLength());
+	int n = A.GetLength() * (A.GetLength() + 1) / 2;
+	for (int i = 0; i < n; i++)
 	{
-		if (length == 0)
-			S.vector = 0;
-		else
-		{
-			S.length = length;
-			int n = length * (length + 1) / 2;
-			S.vector = new T[n];
-			for (int i = 0; i < n; i++)
-				S.vector[i] = vector[i] + A.vector[i];
-		}
+		T a = GetValue(i) + A.GetValue(i);
+		S->SetValue(i, a);
 	}
-	else
-		throw 1;
-	return S;
+	
+	return *S;
 }
 // ---------------------------------------------------------------------------
+template <class T> 
+bool TMatrix<T>::operator==(const TMatrix<T> &A) 
+{
+	if (length != A.length)
+		return false;
+	int f = 0;
+	int n = length * (length + 1) / 2;
+	for (int i = 0; i < n; i++)
+		if (vector[i] != A.vector[i])
+			f = 1;
+	if (f == 1)
+		return false;
+	return true;
+} 
+//-----------------------------------------------------------------------------
 template <class T>
 TMatrix<T> TMatrix<T>::operator-(TMatrix<T> &A)
 {
@@ -97,7 +126,20 @@ TMatrix<T> TMatrix<T>::operator-(TMatrix<T> &A)
 template <class T>
 TMatrix<T>  TMatrix<T>::operator*(TMatrix<T> &A)
 {
-	
+	TMatrix<T> result(length);
+	for (int i = 0; i < length; ++i)
+	{
+		for (int j = i; j < length; ++j)
+		{
+			int sum = 0;
+			for (int k = i; k <= j; k++)
+			{
+				sum += element(i, k)*A.element(k, j);
+			}
+			result.element(i, j) = sum;
+		}
+	}
+	return result;
 }
 // ---------------------------------------------------------------------------
 template <class T>
@@ -105,7 +147,7 @@ TMatrix<T>& TMatrix<T>::operator=(TMatrix<T> &A)
 {
 	if (this != &A)
 	{
-		length = A.length;
+		length = A.GetLength();
 		if (length != 0)
 		{
 			if (vector != 0)
@@ -128,8 +170,8 @@ TMatrix<T>& TMatrix<T>::operator=(TMatrix<T> &A)
 template <class T>
 std::istream& operator>>(std::istream &A, TMatrix<T> &B)
 {
-	A >> B.length;
-	int n = length * (length + 1) / 2;
+	A >> B.GetLength();
+	int n = B.GetLength() * (B.GetLength() + 1) / 2;
 	B.vector = new T[n];
 	for (int i = 0; i < n; i++)
 	{
@@ -141,10 +183,17 @@ std::istream& operator>>(std::istream &A, TMatrix<T> &B)
 template <class T>
 std::ostream& operator<<(std::ostream &A, TMatrix<T> &B)
 {
-	A << B.length << "\n";
-	int n = length * (length + 1) / 2;
+	int a = B.GetLength();
+	printf("%d", a);
+	A << "\n";
+	int n = B.GetLength() * (B.GetLength() + 1) / 2;
 	for (int i = 0; i < n; i++)
-		A << B.vector[i] << "\n";
+	{
+		int b = B.GetValue(i);
+		printf("%d", b);
+		A << "\n";
+	}
+		
 	return A;
 }
 // ---------------------------------------------------------------------------
