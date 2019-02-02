c1dffbf85a06c3c11719e01c6ad398aa2eb3ee5e
3817061Rezantsev <rezantsev.sergey@gmail.com>
2019-02-02 23:40:06 +0300

    report

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -6,12 +6,12 @@ int main()
 {
 	double t1, t2, startMult, endMult, startDel, endDel;
 	cout << "An example of using the Matrix will be implemented here" << endl;
-	TMatrix <int> M1(3), M2(3), M6(1), Res1(3), Res2(3), Res3(3), Res4(3);
+	TMatrix <double> M1(3), M2(3), M6(1), Res1(3), Res2(3), Res3(3), Res4(3);
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3 - i; j++)
 			{
-				M1[i][j] = 1;
-				M2[i][j] = 1;	
+				M1[i][j] = 1+i;
+				M2[i][j] = 2+2*i;	
 			}
 	Res1 = M1 + M2;
 	Res2 = M1 - M2;
@@ -21,9 +21,8 @@ int main()
 	startMult = clock();
 	Res4 = M1 * M2;
 	t1 = (clock() - startMult) / CLOCKS_PER_SEC;
-	cout << M6 << endl;
-	cout << "M1/n" << endl << M1 << endl;
-	cout << "M2/n" << endl << M2 << endl;
+	cout << "M1" << endl << M1 << endl;
+	cout << "M2" << endl << M2 << endl;
 	cout << "M1 + M2" << endl << Res1 << endl;
 	cout << "M1 - M2" << endl << Res2 << endl;
 	cout << "M1 / M2" << endl << Res3 << endl;
