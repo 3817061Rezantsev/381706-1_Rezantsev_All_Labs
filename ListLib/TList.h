5d29df28f6aba3bfb0e520ee121e5154266dbc88
3817061Rezantsev <rezantsev.sergey@gmail.com>
2019-02-03 16:13:37 +0300

    report

---
+++ ListLib/TList.h
@@ -0,0 +1,275 @@
+#pragma once
+#include <iostream>
+#include "TNode.h"
+#include "../Exception/Exception.h"
+using namespace std;
+template <class T>
+class TList
+{
+protected:
+	TNode<T>* list;
+public:
+	TList();
+	TList(TList<T> &list2);
+	void PutBegin(T A);
+	void PutAfter(T A, TNode<T>* node);
+	void PutEnd(T A);
+	int GetSize();
+	TNode<T>* Search(T A);
+	void Delete(T A);
+	void Clean();
+	T GetBegin();
+	T GetCurrent(TNode<T>* A);
+	T GetEnd();
+	bool IsFull();
+	bool IsEmpty();
+	template <class T> friend ostream & operator<<(ostream &out, const TList<T> &B);
+};
+//-----------------------------------------------------------
+template <class T>
+TList<T>::TList()
+{
+	list = NULL;
+}
+//-----------------------------------------------------------
+template <class T>
+TList<T>::TList(TList<T> &list2)
+{
+	if (list2.list)
+	{
+		list = new TNode<T>(list2.list->data, list2.list);
+		TNode<T> *u = list;
+		TNode<T> *u2 = list2.list->next;
+		while (u2 != NULL)
+		{
+			u->next = new TNode<T>(u2->data, u2->next);
+			u = u->next;
+			u2 = u2->next;
+		}
+	}
+	else
+		list = list2.list;;
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::PutBegin(T A)
+{
+	if (this->IsFull())
+		throw TException("Error. List is full");
+	if (list == 0)
+	{
+		TNode<T>* temp = new TNode<T>(A, NULL);
+		list = temp;
+	}
+	else
+	{
+		TNode<T>* temp = new TNode<T>(A, list);
+		list = temp;
+	}
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::PutAfter(T A, TNode<T>* node)
+{
+	if (this->IsFull())
+		throw TException("Error. List is full");
+	if (node && list)
+	{
+		TNode<T>* u = node->next;
+		node->next = new TNode<T>(A, u);
+	}
+	else
+		throw TException("Error. Uncorrect info");
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::PutEnd(T A)
+{
+	if (this->IsFull())
+		throw TException("Error. List is full");
+	if (list != 0)
+	{
+		TNode<T> *B = list;
+		while (B->GetNext() != 0)
+			B = B->GetNext();
+		B->SetNext(new TNode<T>(A, NULL));
+	}
+	else
+		list = new TNode<T>(A, NULL);
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::Delete(T A)
+{
+	TNode<T>* u = list;
+	TNode<T>* u2 = u;
+	if (u)
+	{
+		while ((u->next) && (u->data != A))
+		{
+			u2 = u;
+			u = u->next;
+		}
+
+		if (u->data == A)
+		{
+			u2->next = u->next;
+			if (u == list)
+				list = list->next;
+			delete u;
+		}
+	}
+
+}
+//-----------------------------------------------------------
+template <class T>
+int TList<T>::GetSize()
+{
+	int res = 0;
+	TNode<T>* u = list;
+	while (u)
+	{
+		res++;
+		u = u->next;
+	}
+	return res;
+}
+//-----------------------------------------------------------
+template <class T>
+TNode<T>* TList<T>::Search(T A)
+{
+	TNode<T>* u = list;
+	while (u)
+	{
+		if (u->data == A)
+			return u;
+		u = u->GetNext();
+	}
+	return NULL;
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::Clean()
+{
+	TNode<T>* u = list;
+	TNode<T>* u2;
+	if (u != NULL)
+	{
+		while (u->next != NULL)
+		{
+			u2 = u->next;
+			delete u;
+			u = u2;
+		}
+		delete u;
+		list = NULL;
+	}
+}
+//-----------------------------------------------------------
+template <class T> ostream & operator<<(ostream &out, const TList<T> &B)
+{
+	TNode<T> * u = B.list;
+	while (u)
+	{
+		out << u->data << " ";
+		u = u->next;
+	}
+	return out;
+}
+//-----------------------------------------------------------
+template <class T>
+T TList<T>::GetBegin()
+{
+	TList<T> a = *this;
+	int b = a.GetSize();
+	if (b == 0)
+		throw TException("Error. List is empty");
+	else
+	{
+		TNode<T> *A = list;
+		T temp = list->TNode<T>::GetData();
+		list = list->TNode<T>::GetNext();
+		delete A;
+		return temp;
+	}
+}
+//-----------------------------------------------------------
+template <class T>
+T TList<T>::GetCurrent(TNode<T>* A)
+{
+	if (A && list)
+	{
+		TNode<T>* B = list;
+		while (A != list)
+		{
+			list = list->TNode<T>::GetNext();
+		}
+		T temp = list->TNode<T>::GetData();
+		list = B;
+		return temp;
+	}
+	else
+		throw TException("Error. Uncorrect info");
+}
+//-----------------------------------------------------------
+template <class T>
+T TList<T>::GetEnd()
+{
+	if (list != 0)
+	{
+		TNode<T> *B = list->GetNext();
+		TNode<T> *A = list;
+		if (B == 0)
+		{
+			T temp = A->TNode<T>::GetData();
+			delete A;
+			list = 0;
+			return temp;
+		}
+		else
+		{
+			while (B->GetNext() != 0)
+			{
+				A = B;
+				B = B->GetNext();
+			}
+			T temp = B->GetData();
+			delete B;
+			A->SetNext(0);
+			return temp;
+		}
+	}
+	else
+		throw TException("Error. List is empty");
+}
+//-----------------------------------------------------------
+template <class T>
+bool TList<T>::IsFull()
+{
+	try
+	{
+		TNode<T>* a = new TNode<T>();
+		if (a == NULL)
+			return 1;
+		else
+		{
+			delete a;
+			return 0;
+		}
+	}
+	catch (...)
+	{
+		return 0;
+	}
+	return true;
+}//-------------------------------------------------------------------------
+template <class T>
+bool TList<T>::IsEmpty()
+{
+	TList<T> a = *this;
+	int b = a.GetSize();
+	if (b == 0)
+		return true;
+	else
+		return false;
+}
--- test/test_list.cpp
+++ test/test_list.cpp
@@ -1,6 +1,50 @@
 #include "../ListLib/TList.h"
 
 #include <gtest.h>
+TEST(TNode, can_get_data)
+{
+	TNode<int> el(3, 0);
+	ASSERT_EQ(el.GetData(), 3);
+}
+
+TEST(TNode, can_get_next)
+{
+	TNode<int>* a = 0;
+	TNode<int> el(3, a);
+	ASSERT_EQ(el.GetNext(), a);
+}
+
+TEST(TNode, can_create_elem_default)
+{
+	TNode<int> el;
+	ASSERT_EQ(el.GetData(), 0);
+}
+
+TEST(TNode, can_set_data)
+{
+	TNode<int> el;
+	el.SetData(5);
+	ASSERT_EQ(el.GetData(), 5);
+}
+
+TEST(TNode, can_set_next)
+{
+	TNode<int>* a = 0;
+	TNode<int> el;
+	el.SetNext(a);
+
+	ASSERT_EQ(el.GetNext(), a);
+}
+
+TEST(TNode, can_copy_elem)
+{
+	TNode<int> el(2, 0);
+	TNode<int> el2(el);
+
+	ASSERT_EQ(el.GetNext(), el2.GetNext());
+	ASSERT_EQ(el.GetData(), el2.GetData());
+}
+
 TEST(TList, can_create_list)
 {
 	ASSERT_NO_THROW(TList<int> bf);
@@ -36,3 +80,174 @@ TEST(TList, can_delete)
 	bf.Delete(0);
 	EXPECT_EQ(0, bf.GetSize());
 }
+
+TEST(TList, can_check_empty_true)
+{
+	TList<int> list;
+	ASSERT_TRUE(list.IsEmpty());
+}
+
+TEST(TList, can_check_empty_false)
+{
+	TList<int> list;
+	list.PutBegin(1);
+	ASSERT_FALSE(list.IsEmpty());
+}
+
+TEST(TList, can_check_full_false)
+{
+	TList<int> list;
+	ASSERT_FALSE(list.IsFull());
+}
+
+TEST(TList, can_get_begin)
+{
+	TList<int> list;
+	list.PutBegin(1);
+	list.PutBegin(2);
+	list.PutBegin(3);
+
+	ASSERT_EQ(list.GetBegin(), 3);
+	ASSERT_EQ(list.GetBegin(), 2);
+	ASSERT_EQ(list.GetBegin(), 1);
+}
+
+TEST(TList, can_get_end)
+{
+	TList<int> list;
+	list.PutBegin(3);
+	list.PutBegin(2);
+
+	ASSERT_EQ(list.GetEnd(), 3);
+}
+
+TEST(TList, no_Throw_when_create_copy_of_the_list)
+{
+	TList<int> otherlist;
+	ASSERT_NO_THROW(TList<int> list(otherlist));
+}
+
+TEST(TList, can_put_begin)
+{
+	TList<int> list;
+	list.PutBegin(3);
+	list.PutBegin(2);
+	list.PutBegin(1);
+
+	ASSERT_EQ(list.GetBegin(), 1);
+	ASSERT_EQ(list.GetBegin(), 2);
+	ASSERT_EQ(list.GetBegin(), 3);
+}
+
+TEST(TList, can_put_end)
+{
+	TList<int> list;
+	list.PutEnd(1);
+	list.PutEnd(2);
+	list.PutEnd(3);
+
+	ASSERT_EQ(list.GetBegin(), 1);
+	ASSERT_EQ(list.GetBegin(), 2);
+	ASSERT_EQ(list.GetBegin(), 3);
+}
+
+TEST(TList, can_get_size_no_zero)
+{
+	TList<int> list;
+	list.PutBegin(1);
+	list.PutBegin(1);
+
+	ASSERT_EQ(list.GetSize(), 2);
+}
+
+TEST(TList, can_get_size_zero)
+{
+	TList<int> list;
+
+	ASSERT_EQ(list.GetSize(), 0);
+}
+
+TEST(TList, throw_get_elem_to_index_when_list_is_empty)
+{
+	TList<int> list;
+
+	ASSERT_ANY_THROW(list.GetBegin());
+}
+
+TEST(TList, throw_get_elem_to_incorrent_val)
+{
+	TList<int> list;
+	list.PutBegin(1);
+	list.PutBegin(1);
+	list.PutBegin(1);
+
+	ASSERT_ANY_THROW(list.GetCurrent(0));
+}
+
+TEST(TList, can_get_current_elem)
+{
+	TList<int> list;
+	list.PutBegin(1);
+	list.PutBegin(2);
+	list.PutBegin(3);
+	TNode<int>* a = list.Search(2);
+
+	ASSERT_EQ(list.GetCurrent(a), 2);
+}
+
+TEST(TList, throw_put_elem_to_incorrent_val)
+{
+	TList<int> list;
+	list.PutBegin(1);
+	list.PutBegin(1);
+	list.PutBegin(1);
+	TNode<int> a = 1;
+	ASSERT_ANY_THROW(list.PutAfter(0, 0));
+}
+
+TEST(TList, can_put_elem_to_index)
+{
+	TList<int> list;
+	list.PutEnd(1);
+	list.PutEnd(2);
+	list.PutEnd(3);
+	TNode<int>* a = list.Search(1);
+	list.PutAfter(10, a);
+	a = list.Search(10);
+	ASSERT_EQ(list.GetCurrent(a), 10);
+}
+
+TEST(TList, throw_get_begin_and_get_end_when_list_is_empty)
+{
+	TList<int> list;
+
+	ASSERT_ANY_THROW(list.GetBegin());
+	ASSERT_ANY_THROW(list.GetEnd());
+}
+
+TEST(TList, can_copy_list)
+{
+	TList<int> list;
+	list.PutEnd(1);
+	list.PutEnd(2);
+	list.PutEnd(3);
+	TList<int> list2(list);
+
+	ASSERT_EQ(list.GetBegin(), list2.GetBegin());
+	ASSERT_EQ(list.GetBegin(), list2.GetBegin());
+	ASSERT_EQ(list.GetBegin(), list2.GetBegin());
+}
+
+/*TEST(List, can_check_full_true)
+{
+	TList<int> list;
+	try
+	{
+		while (1)
+			list.PutEnd(1);
+	}
+	catch (...)
+	{
+		ASSERT_TRUE(list.IsFull());
+	}
+}*/
