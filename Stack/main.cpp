2c09f461a3fa2e85a9cc0a575c8055161d6ee1af
3817061Rezantsev <rezantsev.sergey@gmail.com>
2019-02-11 19:41:07 +0300

    smth

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include "TStack.h"
+#include "../Exception/Exception.h"
 using namespace std;
 int main()
 {
-	TStack<int> A(5);
-	A.Put(10);
-	A.Put(5);
-	int b = A.Get();
-	cout << "Put 10 then 5" << endl;
-	cout << "Get val" << endl;
-	cout << b << "\n";
-	A.Put(8);
-	b = A.Get();
-	cout << "Put 8" << endl;
-	cout << "Get val" << endl;
-	cout << b << "\n";
+	try
+	{
+		TStack<int> A(5);
+		A.Put(10);
+		A.Put(5);
+		int b = A.Get();
+		cout << "Put 10 then 5" << endl;
+		cout << "Get val" << endl;
+		cout << b << "\n";
+		A.Put(8);
+		b = A.Get();
+		cout << "Put 8" << endl;
+		cout << "Get val" << endl;
+		cout << b << "\n";
+	}
+	catch (TException ex)
+	{
+		ex.Show();
+	}
+	
 
 }
