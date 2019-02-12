e279b9ce9f1c84491fd78adc2171cdf9578408a6
3817061Rezantsev <rezantsev.sergey@gmail.com>
2019-02-12 17:46:23 +0300

    smth

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -4,25 +4,25 @@
 using namespace std;
 int main()
 {
-	try
-	{
-		TStack<int> A(5);
-		A.Put(10);
-		A.Put(5);
-		int b = A.Get();
-		cout << "Put 10 then 5" << endl;
-		cout << "Get val" << endl;
-		cout << b << "\n";
-		A.Put(8);
-		b = A.Get();
-		cout << "Put 8" << endl;
-		cout << "Get val" << endl;
-		cout << b << "\n";
-	}
-	catch (TException ex)
-	{
-		ex.Show();
-	}
-	
+  try
+  {
+    TStack<int> A(5);
+    A.Put(10);
+    A.Put(5);
+    int b = A.Get();
+    cout << "Put 10 then 5" << endl;
+    cout << "Get val" << endl;
+    cout << b << "\n";
+    A.Put(8);
+    b = A.Get();
+    cout << "Put 8" << endl;
+    cout << "Get val" << endl;
+    cout << b << "\n";
+  }
+  catch (TException ex)
+  {
+    ex.Show();
+  }
+
 
 }
