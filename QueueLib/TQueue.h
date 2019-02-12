afc70ff942760f5af5a7e61d3a8030c0ee0e0d5d
3817061Rezantsev <rezantsev.sergey@gmail.com>
2019-02-12 18:01:06 +0300

    smth

--- QueueLib/TQueue.h
+++ QueueLib/TQueue.h
@@ -7,103 +7,108 @@ template <class T>
 class TQueue :public TStack<T>
 {
 protected:
-	int start;
-	int count;
+  int start;
+  int count;
 public:
-	TQueue(int n);
-	TQueue(TQueue<T> &A);
-	void Put(T A);
-	T Get();
-	bool IsFull();
-	bool IsEmpty();
+  TQueue(int n);
+  TQueue(TQueue<T> &A);
+  virtual ~TQueue();
+  void Put(T A);
+  T Get();
+  bool IsFull();
+  bool IsEmpty();
 };
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TQueue<T>::IsEmpty()
 {
-	if (count == 0)
-		return true;
-	else
-		return false;
+  if (count == 0)
+    return true;
+  else
+    return false;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TQueue<T>::IsFull()
 {
-	if (count == this->size)
-		return true;
-	else
-		return false;
+  if (count == this->size)
+    return true;
+  else
+    return false;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 T TQueue<T>::Get()
 {
-	if (IsEmpty())
-		throw TException("Error.Queue is empty");
-	else 
-	{
-		T res = this->m[start];
-		start++;
-		count--;
-		if (start > this->size - 1)
-			start = 0;
-		return res;
-	}
+  if (IsEmpty())
+    throw TException("Error.Queue is empty");
+  else
+  {
+    T res = this->m[start];
+    start++;
+    count--;
+    if (start > this->size - 1)
+      start = 0;
+    return res;
+  }
 
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TQueue<T>::TQueue(int n) : TStack<T>(n)
 {
-	if (n < 0)
-		throw TException("Error. Wrong size");
-	else if (n == 0)
-	{
-		start = 0;
-		this->size = 0;
-		count = 0;
-		this->r = 0;
-		this->m = 0;
-	}
-	if (n > 0)
-	{
-		start = 0;
-		count = 0;
-		this->size = n;
-		this->m = new T[this->size];
-		for (int i = 0; i < this->size; i++)
-			this->m[i] = 0;
-	}
+  if (n < 0)
+    throw TException("Error. Wrong size");
+  else if (n == 0)
+  {
+    start = 0;
+    this->size = 0;
+    count = 0;
+    this->r = 0;
+    this->m = 0;
+  }
+  if (n > 0)
+  {
+    start = 0;
+    count = 0;
+    this->size = n;
+    this->m = new T[this->size];
+    for (int i = 0; i < this->size; i++)
+      this->m[i] = 0;
+  }
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TQueue<T>::TQueue(TQueue<T> &A) : TStack<T>(A)
 {
-	start = A.start;
-	count = A.count;
-	this->size = A.size;
-	this->r = A.r;
-	if (this->size == 0)
-		this->m = 0;
-	else
-	{
-		this->m = new T[this->size];
-		for (int i = 0; i < this->size;i++)
-			this->m[i] = A.m[i];
-	}
+  start = A.start;
+  count = A.count;
+  this->size = A.size;
+  this->r = A.r;
+  if (this->size == 0)
+    this->m = 0;
+  else
+  {
+    this->m = new T[this->size];
+    for (int i = 0; i < this->size;i++)
+      this->m[i] = A.m[i];
+  }
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 void TQueue<T>::Put(T A)
 {
-	if (IsFull())
-		throw TException("Error. Queue is full.");
-	else 
-	{
-		this->m[this->r++] = A;
-		count++;
-		if (this->r > this->size - 1)
-			this->r = 0;
-	}
+  if (IsFull())
+    throw TException("Error. Queue is full.");
+  else
+  {
+    this->m[this->r++] = A;
+    count++;
+    if (this->r > this->size - 1)
+      this->r = 0;
+  }
 }
+//-------------------------------------------------------------------------------------------------
+template <class T>
+TQueue<T>::~TQueue()
+{}
--- StackLib/TStack.h
+++ StackLib/TStack.h
@@ -5,90 +5,90 @@ template <class T>
 class TStack
 {
 protected:
-	int size;
-	int r;
-	T* m;
+  int size;
+  int r;
+  T* m;
 public:
-	TStack(int n);
-	TStack(TStack<T> &A);
-	void Put(T A);
-	T Get();
-	bool IsFull();
-	bool IsEmply();
+  TStack(int n);
+  TStack(TStack<T> &A);
+  void Put(T A);
+  T Get();
+  bool IsFull();
+  bool IsEmply();
 };
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TStack<T>::TStack(int n)
 {
-	if (n < 0)
-		throw -1;
-	else if (n == 0)
-	{
-		size = 0;
-		r = 0;
-		m = NULL;
-	}
-	if (n > 0)
-	{
-		r = 0;
-		size = n;
-		m = new T[size];
-	}
+  if (n < 0)
+    throw - 1;
+  else if (n == 0)
+  {
+    size = 0;
+    r = 0;
+    m = NULL;
+  }
+  if (n > 0)
+  {
+    r = 0;
+    size = n;
+    m = new T[size];
+  }
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TStack<T>::TStack(TStack<T> &A)
 {
-	size = A.size;
-	r = A.r;
-	if (size == 0)
-		m = NULL;
-	else
-	{
-		m = new T[size];
-		for (int i = 0; i < size; i++)
-			m[i] = A.m[i];
-	}
+  size = A.size;
+  r = A.r;
+  if (size == 0)
+    m = NULL;
+  else
+  {
+    m = new T[size];
+    for (int i = 0; i < size; i++)
+      m[i] = A.m[i];
+  }
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 void TStack<T>::Put(T A)
 {
-	if (IsFull())
-		throw 0;
-	else
-	{
-		m[r] = A;
-		r++;
-	}
+  if (IsFull())
+    throw 0;
+  else
+  {
+    m[r] = A;
+    r++;
+  }
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 T TStack<T>::Get()
 {
-	if (IsEmply())
-		throw 1;
-	else
-	{
-		r--;
-		return m[r];
-	}
+  if (IsEmply())
+    throw 1;
+  else
+  {
+    r--;
+    return m[r];
+  }
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TStack<T>::IsFull()
 {
-	if (((r >= size)&&(size!=0))||(size == 0))
-		return true;
-	else
-		return false;
+  if (((r >= size) && (size != 0)) || (size == 0))
+    return true;
+  else
+    return false;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TStack<T>::IsEmply()
 {
-	if (r == 0)
-		return true;
-	else
-		return false;
+  if (r == 0)
+    return true;
+  else
+    return false;
 }
