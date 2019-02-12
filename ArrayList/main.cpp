0553dea6ee45d5fda0f58091c09c94b66cb6e9f4
3817061Rezantsev <rezantsev.sergey@gmail.com>
2019-02-12 18:16:22 +0300

    smth

--- ArrayList/main.cpp
+++ ArrayList/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include "TAList.h"
+#include "../Exception/Exception.h"
 using namespace std;
-int main() 
+int main()
 {
-	setlocale(LC_ALL, "Russian");
-	cout << "Тестирование программ поддержки структуры типа очереди" << endl;
-	TAList <int> L1(5);
-	L1.PutStart(60);
-	L1.PutStart(40);
-	std::cout << "Положили в начало списка 60, потом 40\n";
-	std::cout << "Взяли первое значение\n";
-	std::cout << L1.GetStart() << "\n";
-	std::cout << "Взяли первое значение\n";
-	std::cout << L1.GetStart() << "\n";
-	L1.PutEnd(60);
-	L1.PutEnd(40);
-	std::cout << "Положили в конец списка 60, потом 40\n";
-	std::cout << "Взяли последнее значение\n";
-	std::cout << L1.GetEnd() << "\n";
+  try
+  {
+    setlocale(LC_ALL, "Russian");
+    cout << "Тестирование программ поддержки структуры типа очереди" << endl;
+    TAList <int> L1(5);
+    L1.PutStart(60);
+    L1.PutStart(40);
+    std::cout << "Положили в начало списка 60, потом 40\n";
+    std::cout << "Взяли первое значение\n";
+    std::cout << L1.GetStart() << "\n";
+    std::cout << "Взяли первое значение\n";
+    std::cout << L1.GetStart() << "\n";
+    L1.PutEnd(60);
+    L1.PutEnd(40);
+    std::cout << "Положили в конец списка 60, потом 40\n";
+    std::cout << "Взяли последнее значение\n";
+    std::cout << L1.GetEnd() << "\n";
+  }
+  catch (TException a)
+  {
+    a.Show();
+  }
+
 }
--- QueueLib/TQueue.h
+++ QueueLib/TQueue.h
@@ -7,86 +7,86 @@ template <class T>
 class TQueue :public TStack<T>
 {
 protected:
-	int start;
-	int count;
+  int start;
+  int count;
 public:
-	TQueue();
-	TQueue(int n);
-	TQueue(TQueue<T> &A);
-	void Put(T A);
-	T Get();
-	bool IsFull();
-	bool IsEmpty();
+  TQueue();
+  TQueue(int n);
+  TQueue(TQueue<T> &A);
+  void Put(T A);
+  T Get();
+  bool IsFull();
+  bool IsEmpty();
 };
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TQueue<T>::IsEmpty()
 {
-	if (count == 0)
-		return true;
-	else
-		return false;
+  if (count == 0)
+    return true;
+  else
+    return false;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TQueue<T>::IsFull()
 {
-	if (count == this->size)
-		return true;
-	else
-		return false;
+  if (count == this->size)
+    return true;
+  else
+    return false;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 T TQueue<T>::Get()
 {
-	if (IsEmpty())
-		throw TException("Error.Queue is empty");
-	else 
-	{
-		T res = this->m[start];
-		start++;
-		count--;
-		if (start > this->size - 1)
-			start = 0;
-		return res;
-	}
+  if (IsEmpty())
+    throw TException("Error.Queue is empty");
+  else
+  {
+    T res = this->m[start];
+    start++;
+    count--;
+    if (start > this->size - 1)
+      start = 0;
+    return res;
+  }
 
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TQueue<T>::TQueue(int n) : TStack<T>(n)
 {
-	start = 0;
-	count = 0;
+  start = 0;
+  count = 0;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TQueue<T>::TQueue() : TStack<T>(0)
 {
-	start = 0;
-	count = 0;
+  start = 0;
+  count = 0;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TQueue<T>::TQueue(TQueue<T> &A) : TStack<T>(A)
 {
 
-	start = A.start;
-	count = A.count;
-	
+  start = A.start;
+  count = A.count;
+
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 void TQueue<T>::Put(T A)
 {
-	if (IsFull())
-		throw TException("Error. Queue is full.");
-	else 
-	{
-		this->m[this->r++] = A;
-		count++;
-		if (this->r > this->size - 1)
-			this->r = 0;
-	}
+  if (IsFull())
+    throw TException("Error. Queue is full.");
+  else
+  {
+    this->m[this->r++] = A;
+    count++;
+    if (this->r > this->size - 1)
+      this->r = 0;
+  }
 }
